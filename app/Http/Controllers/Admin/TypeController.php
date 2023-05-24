2d2ccd867d4c28bab687d65ad13dd9181b9e44e6
lucacappello79 <postt79@gmail.com>
2023-05-24 15:43:14 +0200

    feat: adding type show

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -48,6 +48,7 @@ class TypeController extends Controller
      */
     public function show(Type $type)
     {
+        return view('admin.types.show', compact('type'));
     }
 
     /**
--- resources/views/admin/types/show.blade.php
+++ resources/views/admin/types/show.blade.php
@@ -4,35 +4,36 @@
   
 <div class="container">
     
-<h1 class="m-3">Type Controller</h1>
+<h1 class="m-3">Type: {{$type->name}}</h1>
 
 
-
+@if (count($type->projects) > 0)
 
 <table class="mt-5 table table-striped">
+
     <thead class="thead-dark">
         <tr>
             <th>Name</th>
-            <th>Description</th>
             <th>Slug</th>
-            <th>Total</th>
         </tr>
     </thead>
+
     <tbody>
-        @foreach($types as $item)
+        @foreach($type->projects as $item)
         <tr>
             <td>{{$item->name}}</td>
-            <td>{{$item->description}}</td>
             <td>{{$item->slug}}</td>
-            <td>{{count($item->projects)}}</td>
-        </tr>
+        </tr>           
         @endforeach
     </tbody>
+
 </table>
-
-
-
+    
+@else
+    <strong>No project found</strong>
+@endif
 
 </div>
- 
 @endsection
+
+
